75aafec462b257fbd06a28b78d42a2ceef46ef85
Sayeed <sayeedssaadman272491@gmail.com>
2023-09-08 23:14:16 +0600

    saved

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,47 @@
-void main()
+import 'package:flutter/material.dart';
+void main ()
 {
-
+  runApp(Myapp());
+}
+class Myapp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return  MaterialApp(
+      debugShowCheckedModeBanner:true,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title:  Text("Profile",style: TextStyle(
+          ),),
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // edit with main axis
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.account_circle_rounded,
+                  size: 50,
+                  color: Colors.green,
+                ),
+                Text("Jhon Doe",style: TextStyle(
+                    color: Colors.green,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 20
+                ),),
+                Text("Flutter Batch 4",style: TextStyle(
+                  color: Colors.cyan,
+                  fontSize: 15,
+                  fontWeight: FontWeight.normal
+                ),),
+              ],
+            )),
+      ),
+      // title: "Valley",
+    );
+  }
 }
