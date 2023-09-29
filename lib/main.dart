5897206b12678505d1b393cab6f76fd2eaffcff7
Sayeed <sayeedssaadman272491@gmail.com>
2023-09-29 23:21:20 +0600

    saved

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,84 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter App',
-      home: CounterScreen(),
+      title: 'Image Feed',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: ImageFeedScreen(),
     );
   }
 }
 
-class CounterScreen extends StatefulWidget {
-  @override
-  CounterScreenState createState() => CounterScreenState();
-}
-
-class CounterScreenState extends State<CounterScreen>
-{
-  int count = 0;
-  void incount() {
-    setState(() {
-      count++;
-      if (count == 5)
-      {
-        Dialogshow();
-      }
-    });
-  }
-
-  void decount() {
-    setState(() {
-      if (count > 0) {
-        count--;
-      }
-    });
-  }
-
-  void Dialogshow() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Button Pressed $count times'),
-          actions:
-          [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Close'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+class ImageFeedScreen extends StatelessWidget {
+  final List<String> images = [
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0"
+    // Add more image URLs here
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter App'),
+        title: Text('News Feed'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Count:',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 10),
-            Text(
-              '$count',
-              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:
-            [
-            ElevatedButton(
-            onPressed: incount,
-            child: Text('+',style: TextStyle(fontSize: 30),),
-            ),
-            SizedBox(width: 10),
-            ElevatedButton(
-            onPressed: decount,
-            child: Text('-',style: TextStyle(fontSize: 30),),
-            ),
-            ],
-            ),
-          ],
-        ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return buildImageFeed(orientation);
+        },
       ),
     );
+  }
+
+   buildImageFeed(Orientation orientation) {
+    if (orientation == Orientation.portrait) {
+      return ListView.builder(
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Container(
+              height: 150,
+              width: 150,
+              child: Image.network(images[index]),
+            ),
+          );
+        },
+      );
+    }
+    else {
+      return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child:Container(
+            height: 150,
+            width: 150,
+            child: Image.network(images[index]),
+            ),
+          );
+        },
+      );
+    }
   }
 }
