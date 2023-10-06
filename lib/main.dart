80082b8ee57108ec10d108b1786858e2080f6a32
Sayeed <sayeedssaadman272491@gmail.com>
2023-10-06 23:39:48 +0600

    not completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+//not completed
 void main() {
   runApp(MyApp());
 }
@@ -8,77 +8,95 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image Feed',
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: ImageFeedScreen(),
+      home: MyHomePage(),
     );
   }
 }
 
-class ImageFeedScreen extends StatelessWidget {
-  final List<String> images = [
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0",
-    "https://th.bing.com/th/id/R.c47e7b9f201535c0fd70a3f3b8d9483b?rik=7ccvpcYvXIs%2f7Q&pid=ImgRaw&r=0"
-    // Add more image URLs here
-  ];
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePage createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  Color _selectedColor = Colors.blue;
+  String _selectedSize = 'Small';
+
+  void _changeColorAndShowSnackbar(Color newColor, String newSize) {
+    setState(() {
+      _selectedColor = newColor;
+      _selectedSize = newSize;
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected Size: $_selectedSize'),
+      ),
+    );
+  }
+
+  Widget _buildColorButton(Color color, String size) {
+    return ElevatedButton(
+      onPressed: () {
+        _changeColorAndShowSnackbar(color, size);
+      },
+      style: ElevatedButton.styleFrom(primary: color),
+      child: Text(size),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('News Feed'),
+        title: Text('Size Selector'),
+        centerTitle: true,
       ),
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          return buildImageFeed(orientation);
-        },
+      body: Center(
+        child: Wrap(
+
+          runSpacing: 8,
+          spacing: 8,
+
+          children: [
+            Container(
+              width: 100,
+              height: 40,
+              child: _buildColorButton(Colors.orange, 'S'),
+            ),
+
+            Container(
+              height: 40,
+              width: 100,
+              child: _buildColorButton(Colors.grey, 'M'),
+            ),
+
+            Container(
+              height: 40,
+              width: 100,
+              child: _buildColorButton(Colors.grey, 'L'),
+            ),
+
+            Container(
+              height: 40,
+              width: 100,
+              child: _buildColorButton(Colors.grey, 'XL'),
+            ),
+
+            Container(
+              height: 40,
+              width: 100,
+              child: _buildColorButton(Colors.grey, 'XXL'),
+            ),
+
+            Container(
+              height: 40,
+              width: 100,
+              child: _buildColorButton(Colors.grey, 'XXXL'),
+            ),
+          ],
+        ),
       ),
     );
-  }
-
-   buildImageFeed(Orientation orientation) {
-    if (orientation == Orientation.portrait) {
-      return ListView.builder(
-        itemCount: images.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Container(
-              height: 150,
-              width: 150,
-              child: Image.network(images[index]),
-            ),
-          );
-        },
-      );
-    }
-    else {
-      return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-        ),
-        itemCount: images.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child:Container(
-            height: 150,
-            width: 150,
-            child: Image.network(images[index]),
-            ),
-          );
-        },
-      );
-    }
   }
 }
