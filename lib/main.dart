38a1dc14c027fcd82050522812a14c1c172b521f
Sayeed <sayeedssaadman272491@gmail.com>
2023-11-03 23:25:00 +0600

    not completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'weather_info.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyScreen(),
-    );
-  }
-}
+      home: WeatherInfoMainScreen(),
 
-class MyScreen extends StatefulWidget {
-  @override
-  _MyScreenState createState() => _MyScreenState();
-}
-
-class _MyScreenState extends State<MyScreen> {
-  List<String> items = [
-    "Item 1",
-    "Item 2",
-    "Item 3",
-    "Item 4",
-    "Item 5",
-  ];
-  List<bool> selectedItems = List.filled(5, false);
-
-  void toggleSelection(int index) {
-    setState(() {
-      selectedItems[index] = !selectedItems[index];
-    });
-  }
-
-  void _showSelectedItemsDialog() {
-    int count = selectedItems.where((selected) => selected).length;
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Selected Items"),
-          content: Text("You have selected $count item(s)."),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text("Close"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Selectable List"),
-      ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(items[index]),
-            tileColor: selectedItems[index] ? Colors.blue : null,
-            onTap: () {
-              toggleSelection(index);
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showSelectedItemsDialog();
-        },
-        child: Icon(Icons.check),
-      ),
     );
   }
 }
